67a67e8f972dce9ccee67cc4cb9d809b01d3071b
Christian Wang <christian.gnaw@gmail.com>
2019-04-04 17:28:42 -0400

    Added revised encryption solution

--- hackerrank/encryption.js
+++ hackerrank/encryption.js
@@ -1,8 +1,14 @@
 function encryption(s) {
     let encryp = [];
     s = s.replace(/" "/g, "");
-    let rowLen = Math.floor(Math.sqrt(s.length));
+    let rowLen;
+    if (Math.sqrt(s.length) < Math.floor(Math.sqrt(s.length) + 0.5))
+        rowLen = Math.sqrt(s.length + 1);
+    else
+        rowLen = Math.floor(Math.sqrt(s.length));
     let colLen = Math.ceil(s.length / rowLen);
+
+
     for (let i = 0; i < s.length; i++) {
         if (encryp[i % colLen])
             encryp[i % colLen] += s[i];
