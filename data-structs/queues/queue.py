eb7103893ba084a301d23bfa2de0f7f23420e5cf
Christian Wang <christian.gnaw@gmail.com>
2019-11-21 21:14:35 -0500

    Started adding data-structs practice

---
+++ data-structs/queues/queue.py
@@ -0,0 +1,22 @@
+# LinkedList Node
+class Node:
+    # Defines a new node passing in a value and next node reference
+    def __init__(self, value, ref=None):
+        self.value=value
+        self.next=ref
+
+# Implementation of Queue using a LinkedList
+class Queue:
+    def __init__(self):
+        self.head=None
+        self.size=0
+    
+    def push(self, value):
+        def aux(node):
+            if(node.next == None):
+                n = Node(value)
+                node.next=n
+            return aux(node.next)
+        return aux(self.head)
+
+    def peek()
---
+++ data-structs/stacks/stack.py
@@ -0,0 +1,28 @@
+# LinkedList Node
+class Node:
+    # Defines a new node passing in a value and next node reference
+    def __init__(self, value, ref=None):
+        self.value = value
+        self.next = ref
+
+# Implementation of Stack using a LinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def push(self, value):
+        n = Node(value, self.head)
+        self.head = n
+        self.size += 1
+
+    def peek(self):
+        return self.head.value
+
+    def pop(self):
+        val = self.head.value
+        self.head = self.head.next
+        self.size -= 1
+        return val
